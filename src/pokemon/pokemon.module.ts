0641c35066aab99d345b29a53720bc713a65b743
Santiago Reyes <santiago.reyes@platinum.net.co>
2026-04-22 09:02:08 -0500

    finish section 7 - start section 8 seeder and paginate

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,17 +1,19 @@
-import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
-import { PokemonController } from './pokemon.controller';
+import {Module} from '@nestjs/common';
+import {PokemonService} from './pokemon.service';
+import {PokemonController} from './pokemon.controller';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Pokemon, PokemonSchema} from "./entities/pokemon.entity";
 
 @Module({
-  imports: [
-      MongooseModule.forFeature([{
-        name: Pokemon.name,
-        schema: PokemonSchema
-      }])
-  ],
-  controllers: [PokemonController],
-  providers: [PokemonService],
+    imports: [
+        MongooseModule.forFeature([{
+            name: Pokemon.name,
+            schema: PokemonSchema
+        }])
+    ],
+    controllers: [PokemonController],
+    providers: [PokemonService],
+    exports: [PokemonService]
 })
-export class PokemonModule {}
+export class PokemonModule {
+}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
+import axios, {AxiosInstance} from "axios";
+import {PokemonResponseInterface} from "../common/interfaces/pokemonResponse.interface";
+import {PokemonService} from "../pokemon/pokemon.service";
 
 @Injectable()
 export class SeedService {
-  execute() {
-       return `Seed executed successfully`
-   }
+    private readonly axios: AxiosInstance = axios;
+    constructor(private readonly pokemonService: PokemonService) {}
+
+    async execute() {
+        try {
+            const {data} = await this.axios.get<PokemonResponseInterface>('https://pokeapi.co/api/v2/pokemon?limit=650')
+            data.results.forEach(pokemon => {
+                const segments = pokemon.url.split('/');
+                const id = +segments[segments.length - 2];
+                this.pokemonService.create({name: pokemon.name, id});
+            })
+            return 'seed executed successfully';
+        }catch (e) {
+            console.log('Error message: ', e.message);
+            throw new BadRequestException('Error executing seed');
+        }
+    }
 }
